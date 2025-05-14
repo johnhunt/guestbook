4aa03cf91590910ccc72cb80f9dd380c6cbe1451
John Hunt <johnrhunt@gmail.com>
2025-05-14 11:34:03 +0100

    Make API run migrations on startup

--- src-api/main.py
+++ src-api/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
+from alembic import command
+from alembic.config import Config
+import os
+
+def run_migrations():
+    alembic_ini_path = os.path.join(os.path.dirname(__file__), "alembic.ini")
+    alembic_cfg = Config(alembic_ini_path)
+    alembic_cfg.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+    command.upgrade(alembic_cfg, "head")
 
 app = FastAPI()
+
+# Run the alembic migrations on startup
+@app.on_event("startup")
+async def startup():
+    run_migrations()
 
 @app.get(
     "/",
